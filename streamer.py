181c5a756062018223828e1257f6cc60fca8d1cf
Dennis Angelov <denangelov123@gmail.com>
2021-02-18 15:56:16 -0600

    Stage3b done

--- streamer.py
+++ streamer.py
@@ -3,7 +3,8 @@ from lossy_socket import LossyUDP
 # do not import anything else from socket except INADDR_ANY
 from socket import INADDR_ANY
 import struct
-
+import concurrent.futures
+import time
 
 class Streamer:
     def __init__(self, dst_ip, dst_port,
@@ -14,57 +15,81 @@ class Streamer:
         self.socket.bind((src_ip, src_port))
         self.dst_ip = dst_ip
         self.dst_port = dst_port
+
         self.recv_buff = {}
-        self.curr_sequence_num = 0
-        self.packet_num = 0
+        self.incoming_sequence_num = 0
+        self.outbound_sequence_num = 0
 
-    def send(self, data_bytes: bytes) -> None:
-        """Note that data_bytes can be larger than one packet."""
-        byte_size = len(data_bytes)
-        # print('data', data_bytes)
-        # print('len', byte_size)
-        if byte_size < 1468:
-            # print('packet', struct.pack('i{}s'.format(byte_size), self.packet_num, data_bytes))
-            self.socket.sendto(struct.pack('i{}s'.format(byte_size), self.packet_num, data_bytes), (self.dst_ip, self.dst_port))
-            self.packet_num = self.packet_num + 1
-        else:
-            while len(data_bytes) > 1468:
-                packet = struct.pack('i 1468s', self.packet_num, data_bytes[:1468])
-                self.socket.sendto(packet, (self.dst_ip, self.dst_port))
-                data_bytes = data_bytes[1468:]
-                self.packet_num = self.packet_num + 1
-            
-            self.socket.sendto(struct.pack('i{}s'.format(len(data_bytes)), self.packet_num, data_bytes), (self.dst_ip, self.dst_port))
+        self.closed = False
+        self.ack = False
         
+        executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+        executor.submit(self.listener)
+
+    def listener(self):
+        while not self.closed:
+            try:
+                data, addr = self.socket.recvfrom()
+
+                recv_packet_num = struct.unpack('i', data[0:4])[0]
+                is_ack = struct.unpack('i', data[4:8])[0]
+                
+                # Case1: The received packet is an ACK
+                if is_ack:
+                    self.ack = True
+
+                # Case2: The received packet is normal data that we must add to the recv buffer
+                else:
+                    data_bytes = data[8:]
+
+                    # Add the data to the receive buffer
+                    self.recv_buff[recv_packet_num] = data_bytes
+
+                    # Send an ACK back
+                    self.send(b'', 1)
+
+            except Exception as e:
+                print("listener died!")
+                print(e)
+
+    def send(self, data_bytes: bytes, is_ack =0) -> None:
+        if is_ack:
+            self.socket.sendto(struct.pack('ii', self.outbound_sequence_num, is_ack), (self.dst_ip, self.dst_port))
+            return
+
+        header = struct.pack('ii', self.outbound_sequence_num, is_ack)
+
+        # Break up the data into chunks of 1464 bytes and send it
+        while len(data_bytes) > 1464:
+            packet = header + data_bytes[0:1464]
+
+            self.socket.sendto(packet, (self.dst_ip, self.dst_port))
+            data_bytes = data_bytes[1464:]
+            self.outbound_sequence_num = self.outbound_sequence_num + 1
+
+            self.ack = False
+
+            # Waiting to receive an ACK for the sent data before the send is "completed"
+            while not self.ack:
+                time.sleep(0.01)
+
+        # Sending the last packet of data of len <= 1464 bytes
+        packet = header + data_bytes
+        self.socket.sendto(packet, (self.dst_ip, self.dst_port))
+        self.ack = False
+
+        # Waiting to receive an ACK for the sent data before the send is "completed"
+        while not self.ack:
+            time.sleep(0.01)
+                
     def recv(self) -> bytes:
-        """Blocks (waits) if no data is ready to be read from the connection."""
-        while True:
-            data, addr = self.socket.recvfrom()
-            # print('packet_received', data)
-            p_num, data_bytes = struct.unpack('i', data[:4]), data[4:]
-            packet = struct.unpack('{}s'.format(len(data_bytes)), data_bytes)
-            
-            # print('data_bytes', packet)
-            # print('p_num', p_num)
-
-            # print('received packet', p_num)
-            # print('current sequence num is', self.curr_sequence_num)
-
-            if self.curr_sequence_num == p_num[0]:
-                print('here1')
-                self.curr_sequence_num = self.curr_sequence_num + 1
-                return packet[0]
-            
-            self.recv_buff[p_num[0]] = packet[0]
-            
-            if self.curr_sequence_num in list(self.recv_buff):
-                print('here2')
-                key = self.curr_sequence_num
-                self.curr_sequence_num = self.curr_sequence_num + 1
-                return self.recv_buff[key]
+        while True:      
+            if self.outbound_sequence_num in list(self.recv_buff):
+                data_bytes = self.recv_buff[self.outbound_sequence_num]
+                self.recv_buff.pop(self.outbound_sequence_num)
+                return data_bytes
 
     def close(self) -> None:
-        """Cleans up. It should block (wait) until the Streamer is done with all
-           the necessary ACKs and retransmissions"""
         # your code goes here, especially after you add ACKs and retransmissions.
-        pass
+        self.closed = True
+        self.socket.stoprecv()
