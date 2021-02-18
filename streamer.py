57e0411476ec90b6145fa0e121e0d7952a0396c5
Dennis Angelov <denangelov123@gmail.com>
2021-02-17 18:40:18 -0600

    stage2 done

--- streamer.py
+++ streamer.py
@@ -14,20 +14,54 @@ class Streamer:
         self.socket.bind((src_ip, src_port))
         self.dst_ip = dst_ip
         self.dst_port = dst_port
+        self.recv_buff = {}
+        self.curr_sequence_num = 0
+        self.packet_num = 0
 
     def send(self, data_bytes: bytes) -> None:
         """Note that data_bytes can be larger than one packet."""
-        if len(data_bytes) < 1473:
-            self.socket.sendto(data_bytes, (self.dst_ip, self.dst_port))
+        byte_size = len(data_bytes)
+        # print('data', data_bytes)
+        # print('len', byte_size)
+        if byte_size < 1468:
+            # print('packet', struct.pack('i{}s'.format(byte_size), self.packet_num, data_bytes))
+            self.socket.sendto(struct.pack('i{}s'.format(byte_size), self.packet_num, data_bytes), (self.dst_ip, self.dst_port))
+            self.packet_num = self.packet_num + 1
         else:
-            while data_bytes:
-                self.socket.sendto(data_bytes[:1472], (self.dst_ip, self.dst_port))
-                data_bytes = data_bytes[1472:]
-    
+            while len(data_bytes) > 1468:
+                packet = struct.pack('i 1468s', self.packet_num, data_bytes[:1468])
+                self.socket.sendto(packet, (self.dst_ip, self.dst_port))
+                data_bytes = data_bytes[1468:]
+                self.packet_num = self.packet_num + 1
+            
+            self.socket.sendto(struct.pack('i{}s'.format(len(data_bytes)), self.packet_num, data_bytes), (self.dst_ip, self.dst_port))
+        
     def recv(self) -> bytes:
         """Blocks (waits) if no data is ready to be read from the connection."""
-        data, addr = self.socket.recvfrom()
-        return(data)
+        while True:
+            data, addr = self.socket.recvfrom()
+            # print('packet_received', data)
+            p_num, data_bytes = struct.unpack('i', data[:4]), data[4:]
+            packet = struct.unpack('{}s'.format(len(data_bytes)), data_bytes)
+            
+            # print('data_bytes', packet)
+            # print('p_num', p_num)
+
+            # print('received packet', p_num)
+            # print('current sequence num is', self.curr_sequence_num)
+
+            if self.curr_sequence_num == p_num[0]:
+                print('here1')
+                self.curr_sequence_num = self.curr_sequence_num + 1
+                return packet[0]
+            
+            self.recv_buff[p_num[0]] = packet[0]
+            
+            if self.curr_sequence_num in list(self.recv_buff):
+                print('here2')
+                key = self.curr_sequence_num
+                self.curr_sequence_num = self.curr_sequence_num + 1
+                return self.recv_buff[key]
 
     def close(self) -> None:
         """Cleans up. It should block (wait) until the Streamer is done with all
